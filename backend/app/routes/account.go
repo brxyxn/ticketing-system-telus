2bba71673985eb926017d7072708073cf36c189c
Brayan Lopez <brxyxn.corp@live.com>
2022-05-29 00:04:32 -0600

    updated routes

--- backend/app/routes/account.go
+++ backend/app/routes/account.go
@@ -15,9 +15,15 @@ func AccountRoutes(app *fiber.App, db *sql.DB) {
 	userService := users.NewUserService(userRepo)
 	userHandler := users.NewUserFiberHandler(userService)
 
-	// Accounts
-	api.Post("/register", userHandler.RegisterAccount) // register profile, user and [customer + company | agent + team]
-	api.Post("/login", userHandler.RegisterAccount)    // login user
-	api.Get("/user", userHandler.RegisterAccount)      // returns user profile
-	api.Get("/logout", userHandler.RegisterAccount)    // logout user
+	// Accounts for customers
+	api.Post("/customer/register", userHandler.RegisterAccount) // register profile, user and [customer + company | agent + team]
+	api.Post("/customer/login", userHandler.Authenticate)       // login user
+	api.Get("/customer/user", userHandler.RegisterAccount)      // returns user profile
+
+	// Accounts for agents
+	api.Post("/agent/register", userHandler.RegisterAccount) // register profile, user and [customer + company | agent + team]
+	api.Post("/agent/login", userHandler.Authenticate)       // login user
+	api.Get("/agent/user", userHandler.RegisterAccount)      // returns user profile
+
+	api.Get("/logout", userHandler.RegisterAccount) // logout user
 }
