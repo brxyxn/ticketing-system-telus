5dad30e21edddbe3d150a7d90fdfa286e9bc9a60
Brayan Lopez <brxyxn.corp@live.com>
2022-05-29 17:45:43 -0600

    added repository interface list

--- backend/internal/tickets/repository.go
+++ backend/internal/tickets/repository.go
@@ -1,4 +1,8 @@
 package tickets
 
 type TicketRepository interface {
+	GetTickets() (Tickets, error)               // list all tickets
+	GetTicket(id int64) (Ticket, error)         // get ticket by id
+	CreateTicket(ticket Ticket) (Ticket, error) // create new ticket
+	CloseTicket(ticket Ticket) (Ticket, error)  // update ticket status to closed
 }
